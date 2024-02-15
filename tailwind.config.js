0514c31167ab4e567495dd80a7d58237ba131c82
zhyck1316 <zhyck1316@gmail.com>
2024-02-15 17:00:54 +0800

    End shift Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
-    extend: {},
+    letterSpacing: {
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '.05em',
+      widest: '.1em',
+      widest: '.25em',
+      wide1: '.50em',
+    },
+    fontFamily: {
+      'dankory-font': ['Passion One']
+    },
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+      '6xl': '4.052rem',
+    },
+    extend: {
+      colors: {
+        'dankory-green': '#5E940E',
+      },
+    },
   },
   plugins: [],
 }
